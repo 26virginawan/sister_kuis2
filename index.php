36e46f201c5f891d00ef59a00b996fe9f8b01340
= <=>
2021-12-02 10:43:08 +0700

    menambah dockerfile dan dockercompose

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -29,55 +30,61 @@
 
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
-	<style type="text/css">
-	body {
-	  padding-top: 40px;
-	  padding-bottom: 40px;
-	  background-color: #eee;
-	}
+    <style type="text/css">
+    body {
+        padding-top: 40px;
+        padding-bottom: 40px;
+        background-color: #eee;
+    }
 
-	.form-signin {
-	  max-width: 330px;
-	  padding: 15px;
-	  margin: 0 auto;
-	}
-	.form-signin .form-signin-heading,
-	.form-signin .checkbox {
-	  margin-bottom: 10px;
-	}
-	.form-signin .checkbox {
-	  font-weight: normal;
-	}
-	.form-signin .form-control {
-	  position: relative;
-	  height: auto;
-	  -webkit-box-sizing: border-box;
-		 -moz-box-sizing: border-box;
-			  box-sizing: border-box;
-	  padding: 10px;
-	  font-size: 16px;
-	}
-	.form-signin .form-control:focus {
-	  z-index: 2;
-	}
-	.form-signin input[type="text"] {
-	  margin-bottom: -1px;
-	  border-bottom-right-radius: 0;
-	  border-bottom-left-radius: 0;
-	}
-	.form-signin input[type="password"] {
-	  margin-bottom: 10px;
-	  border-top-left-radius: 0;
-	  border-top-right-radius: 0;
-	}
-	</style>
+    .form-signin {
+        max-width: 330px;
+        padding: 15px;
+        margin: 0 auto;
+    }
 
-  </head>
+    .form-signin .form-signin-heading,
+    .form-signin .checkbox {
+        margin-bottom: 10px;
+    }
 
-  <body>
+    .form-signin .checkbox {
+        font-weight: normal;
+    }
+
+    .form-signin .form-control {
+        position: relative;
+        height: auto;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+        padding: 10px;
+        font-size: 16px;
+    }
+
+    .form-signin .form-control:focus {
+        z-index: 2;
+    }
+
+    .form-signin input[type="text"] {
+        margin-bottom: -1px;
+        border-bottom-right-radius: 0;
+        border-bottom-left-radius: 0;
+    }
+
+    .form-signin input[type="password"] {
+        margin-bottom: 10px;
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+    }
+    </style>
+
+</head>
+
+<body>
 
     <div class="container">
-	<?php
+        <?php
 
     //apabila tombol login di klik akan menjalankan skript dibawah ini
 	if( isset( $_REQUEST['login'] ) ){
@@ -110,30 +117,31 @@
 
 	} else {
 	?>
-      <form class="form-signin" method="post" action="" role="form">
-		<?php
+        <form class="form-signin" method="post" action="" role="form">
+            <?php
 		if(isset($_SESSION['err'])){
 			$err = $_SESSION['err'];
 			echo '<div class="alert alert-warning alert-message">'.$err.'</div>';
             unset($_SESSION['err']);
 		}
 		?>
-        <h2 class="form-signin-heading">Login Admin</h2>
-        <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
-        <input type="password" name="password" class="form-control" placeholder="Password" required>
-        <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Login</button>
-      </form>
-	<?php
+            <h2 class="form-signin-heading">Login Admin</h2>
+            <input type="text" name="username" class="form-control" placeholder="Username" required autofocus>
+            <input type="password" name="password" class="form-control" placeholder="Password" required>
+            <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Login</button>
+        </form>
+        <?php
 	}
 	?>
     </div> <!-- /container -->
 
-	<!-- Bootstrap core JavaScript, Placed at the end of the document so the pages load faster -->
+    <!-- Bootstrap core JavaScript, Placed at the end of the document so the pages load faster -->
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
 
-	<script type="text/javascript">
-		$(".alert-message").alert().delay(3000).slideUp('slow');
-	</script>
-  </body>
+    <script type="text/javascript">
+    $(".alert-message").alert().delay(3000).slideUp('slow');
+    </script>
+</body>
+
 </html>
